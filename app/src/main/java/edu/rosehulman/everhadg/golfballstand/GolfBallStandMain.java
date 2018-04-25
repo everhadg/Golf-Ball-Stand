7f7a16f61f4c3862aec59871856c137ee2d64f73
everhadg <38666807+everhadg@users.noreply.github.com>
2018-04-25 14:47:27 -0400

    Initial commit
    not much to see here

---
+++ app/src/main/java/edu/rosehulman/everhadg/golfballstand/GolfBallStandMain.java
@@ -0,0 +1,13 @@
+package edu.rosehulman.everhadg.golfballstand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GolfBallStandMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_golf_ball_stand_main);
+    }
+}
