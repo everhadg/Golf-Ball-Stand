8b6b362cea14d79d00da8421e05ac8f350a5c3fa
Cade <34425033+monkeycade@users.noreply.github.com>
2018-04-25 19:49:53 -0400

    Add the library + color detect communication

--- app/src/main/java/edu/rosehulman/everhadg/golfballstand/GolfBallStandMain.java
+++ app/src/main/java/edu/rosehulman/everhadg/golfballstand/GolfBallStandMain.java
@@ -1,13 +1,63 @@
 package edu.rosehulman.everhadg.golfballstand;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class GolfBallStandMain extends AppCompatActivity {
+import edu.rosehulman.me435.AccessoryActivity;
+
+public class GolfBallStandMain extends AccessoryActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_golf_ball_stand_main);
+    }
+
+    public void performBallTest(View view) {
+        sendCommand("CUSTOM COLOR DETECT");
+    }
+
+    @Override
+    protected void onCommandReceived(String receivedCommand) {
+        super.onCommandReceived(receivedCommand);
+        Toast.makeText(this, "Received " + receivedCommand, Toast.LENGTH_SHORT);
+        if (receivedCommand.contains("Location")) {
+            String[] temp = receivedCommand.split(":");
+            Toast.makeText(this,temp[1],Toast.LENGTH_SHORT);
+            int location = Integer.parseInt(temp[1]);
+            int ballnum = Integer.parseInt(temp[2]);
+            String ball;
+            switch (ballnum) {
+                case -1:
+                    ball = "no ball";
+                    break;
+                case 0:
+                    ball = "black ball";
+                    break;
+                case 1:
+                    ball = "blue ball";
+                    break;
+                case 2:
+                    ball = "green ball";
+                    break;
+                case 3:
+                    ball = "red ball";
+                    break;
+                case 4:
+                    ball = "yellow ball";
+                    break;
+                case 5:
+                    ball = "white ball";
+                    break;
+                default:
+                    ball = "Error";
+                    break;
+            }
+            //TODO Change the Textview in response to the return ball color
+            Toast.makeText(this,"Location " + location + " has " + ball,Toast.LENGTH_SHORT);
+        }
     }
 }
