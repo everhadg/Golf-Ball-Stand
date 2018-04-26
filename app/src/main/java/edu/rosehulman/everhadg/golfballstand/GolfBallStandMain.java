4160b97aecb801ac8ce26e07f48d1be9b7c99a85
everhadg <38666807+everhadg@users.noreply.github.com>
2018-04-25 21:01:47 -0400

    Updated GUI a bit and implemented changing the TextDisplays for the balls

--- app/src/main/java/edu/rosehulman/everhadg/golfballstand/GolfBallStandMain.java
+++ app/src/main/java/edu/rosehulman/everhadg/golfballstand/GolfBallStandMain.java
@@ -4,16 +4,71 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import edu.rosehulman.me435.AccessoryActivity;
 
 public class GolfBallStandMain extends AccessoryActivity {
 
+    private ToggleButton mToggle1,mToggle2,mToggle3;
+    private TextView mColor1,mColor2,mColor3;
+    private int mBallToChange;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_golf_ball_stand_main);
+
+        mToggle1 = findViewById(R.id.ball1Toggle);
+        mToggle1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    // Toggle is enabled
+                    mToggle2.setChecked(false);
+                    mToggle2.setChecked(false);
+                    setBallToChange(1);
+                } else {
+                    // Toggle is disabled
+                }
+            }
+        });
+        mToggle2 = findViewById(R.id.ball2Toggle);
+        mToggle2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    mToggle1.setChecked(false);
+                    mToggle3.setChecked(false);
+                    setBallToChange(2);
+                    // Toggle is enabled
+                } else {
+                    // Toggle is disabled
+                }
+            }
+        });
+        mToggle3 = findViewById(R.id.ball3Toggle);
+        mToggle3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    // Toggle is enabled
+                    mToggle1.setChecked(false);
+                    mToggle2.setChecked(false);
+                    setBallToChange(3);
+                } else {
+                    // Toggle is disabled
+                }
+            }
+        });
+
+        mColor1 = findViewById(R.id.ball1Label);
+        mColor2 = findViewById(R.id.ball2Label);
+        mColor3 = findViewById(R.id.ball3Label);
+
     }
 
     public void performBallTest(View view) {
@@ -32,32 +87,61 @@ public class GolfBallStandMain extends AccessoryActivity {
             String ball;
             switch (ballnum) {
                 case -1:
-                    ball = "no ball";
+                    ball = "none";
                     break;
                 case 0:
-                    ball = "black ball";
+                    ball = "black";
                     break;
                 case 1:
-                    ball = "blue ball";
+                    ball = "blue";
                     break;
                 case 2:
-                    ball = "green ball";
+                    ball = "green";
                     break;
                 case 3:
-                    ball = "red ball";
+                    ball = "red";
                     break;
                 case 4:
-                    ball = "yellow ball";
+                    ball = "yellow";
                     break;
                 case 5:
-                    ball = "white ball";
+                    ball = "white";
                     break;
                 default:
                     ball = "Error";
                     break;
             }
-            //TODO Change the Textview in response to the return ball color
             Toast.makeText(this,"Location " + location + " has " + ball,Toast.LENGTH_SHORT).show();
+            setBallColorText(location,ball);
         }
+    }
+
+    public void setBallColorText(int location, String color){
+        switch(location) {
+            case 1:
+                mColor1.setText(color);
+                break;
+            case 2:
+                mColor2.setText(color);
+                break;
+            case 3:
+                mColor3.setText(color);
+            default:
+                Toast.makeText(this, "Location Error", Toast.LENGTH_LONG);
+                break;
+        }
+    }
+
+    public void setBallToChange(int location){
+        mBallToChange = location;
+    }
+
+    public void toggleBall1(View view) {
+    }
+
+    public void toggleBall2(View view) {
+    }
+
+    public void toggleBall3(View view) {
     }
 }
